a734ba754d7f1958fa8a514ca5199509ac324f0b
cheng ma <macheng2025@gmail.com>
2019-09-22 16:00:23 +0800

    add 'add' method

--- src/Array.java
+++ src/Array.java
@@ -27,6 +27,34 @@ public class Array {
         return size == 0;
     }
 
+    // 向数组最后添加新元素
+    public void addLastList(int e){
+        if (data.length == size ) {
+            throw new IllegalArgumentException("AddLast failed. Array is full.");
+        }
+        data[size] = e;
+        size ++;
+    }
+
+    public void add (int index, int e){
+        if (data.length == size){
+            throw new IllegalArgumentException("Add value failed ");
+        }
+
+        if (index < 0 || index > size ){
+            throw new IllegalArgumentException("Add value failed");
+        }
+
+        for (int i = size-1; i >= index; i--){
+            data[i+1] = data[i];
+        }
+
+        data[index] = e;
+        size ++;
+
+
+    }
+
     public static void main(String[] args) {
         int arr [] =  new int []{100, 20, 10};
         System.out.println(arr);
