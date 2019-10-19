d1c39b9d8f56f7b1b6847f276bd56624b77f46ce
cheng ma <macheng2025@gmail.com>
2019-10-19 20:25:55 +0800

    在LinkedList当中添加内部类Node

---
+++ src/LinkedList.java
@@ -0,0 +1,26 @@
+public class LinkedList {
+
+    private class Node<E> {
+        private E e;
+        private Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+    }
+
+}
