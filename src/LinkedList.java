6355af744b3eaf16d11f576c78a1fa60747722d3
cheng ma <macheng2025@gmail.com>
2019-10-20 10:53:47 +0800

    在链表中添加元素

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,4 @@
-public class LinkedList {
+public class LinkedList<E> {
 
     private class Node<E> {
         private E e;
@@ -22,5 +22,31 @@ public class LinkedList {
             return super.toString();
         }
     }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 在链表头部添加元素e
+    public void addFirst(E e) {
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+        head = new Node(e, head);
+        size++;
+    }
+
 
 }
