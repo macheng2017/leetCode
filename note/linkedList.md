8e1a875609e435a1b3d99d9f6e0714b12880e19a
cheng ma <macheng2025@gmail.com>
2019-11-03 11:44:33 +0800

    使用链表实现队列

---
+++ note/linkedList.md
@@ -0,0 +1,14 @@
+发现使用github history功能做笔记很好用
+
+以后要经常使用这个功能
+
+
+截图功能 
+
+1. shift + commend + 4
+2. 拖拽到
+
+
+# 使用链表实现队列
+
+![](./img/Evernote Snapshot 20191103 113007.png)
---
+++ src/LinkedListQueue.java
@@ -0,0 +1,51 @@
+public class LinkedListQueue<E> implements Queue<E> {
+
+    class Node<E> {
+        private E e;
+        private Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+
+    @Override
+    public void enqueue(E e) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E getFront() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
